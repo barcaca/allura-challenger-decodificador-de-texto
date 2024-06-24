6831c62b7653f366517ec1bf01506adb99334534
barcaca <73846225+barcaca@users.noreply.github.com>
2024-06-24 16:38:08 -0300

    feat: hmlt/css feito

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
